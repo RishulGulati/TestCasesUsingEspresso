9397cde815797abd1640cfa8cd8f482305fd0f50
RishulGulati <rishul10@gmail.com>
2019-03-10 17:13:12 +0530

    splash time changed

--- app/src/main/java/com/example/testcasesusingespresso/activity/SplashActivity.java
+++ app/src/main/java/com/example/testcasesusingespresso/activity/SplashActivity.java
@@ -9,7 +9,7 @@ import com.example.testcasesusingespresso.R;
 
 public class SplashActivity extends Activity {
 
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 3500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
